2474c4f73695c2314b8b9b75e02b7e67c9852d54
gbeto <gbeto23@gmail.com>
2014-02-25 12:20:03 -0600

    cambié un poquito el index

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -11,6 +11,8 @@
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1" ></li>
       <li data-target="#myCarousel" data-slide-to="2" ></li>
+      <li data-target="#myCarousel" data-slide-to="3" ></li>
+
     </ol>
 
 
@@ -18,25 +20,25 @@
     <div class="item active"><!-- class of active since it's the first item -->
       <img src="assets/img/slide3.png"  alt="" />
       <div class="carousel-caption">
-        <p>Caption text here</p>
+        <p>Sistemas de Ventilación, Presurizadores y mas...</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="assets/img/slide2.png"  alt="" />
+      <div class="carousel-caption">
+        <p>Visítenos en nuestras nuevas instalaciones</p>
       </div>
     </div>
     <div class="item">
       <img src="assets/img/slide1.png"  alt="" />
       <div class="carousel-caption">
-        <p>Caption text here</p>
+        <p>Suscríbase a nuestros boletines y obtenga múltiples beneficios</p>
       </div>
     </div>
     <div class="item">
-      <img src="assets/img/slide1.png"  alt="" />
+      <img src="assets/img/slide4.png"  alt="" />
       <div class="carousel-caption">
-        <p>Caption text here</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="assets/img/slide1.png"  alt="" />
-      <div class="carousel-caption">
-        <p>Caption text here</p>
+        <p>Ahora ya puede comprar en línea</p>
       </div>
     </div>
   </div><!-- /.carousel-inner -->
@@ -50,43 +52,53 @@
      
     <div class="col-md-12"  style="color:#0D3767;">
         <h1>Bienvenido a Grupo Siel</h1>
-        <p>
-
-  <h3 style="color:#131304;">Grupo Siel comenzó en 1988. Además de distribuir marcas lideres en el mercado, prestamos servicios
+        <p class="text-left" style="color:#131304;">
+  Grupo Siel comenzó en 1988. Además de distribuir marcas lideres en el mercado, prestamos servicios
     de asesoría, creación de proyectos y soluciones para cualquier sector, ya sea hábitat, comercial o industrial.
-    No dude en contactarnos, le cotizamos cualquier presupuesto de manera inmediata y sin compromiso alguno.</h3>
+    No dude en contactarnos, le cotizamos cualquier presupuesto de manera inmediata y sin compromiso alguno.
             </p>
     </div>  
 <div class="row">
   <div class="col-md-12 col">
-    <div class="col-md-4 columna" style="background-color:#C8C8C8; text-align:center;">
-      <h3>Productos</h3>
-      <img src="assets/img/columnas.png" max-width="100%" height="100">
-      <p >
+    <div class="col-md-4">
+          <div class="col-md-12 columna" style="background-color:#C8C8C8;">
 
- Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-          <center> <a class="btn btn-primary btn-sm">ver más...</a></center>
+              <h3 class="text-center">Productos</h3>
+              <img class="img-responsive"  src="assets/img/columnas.png" max-width="100%" height="auto">
+              <p class="text-center">
 
-      </p>
-
-    </div>
-    <div class="col-md-4 columna" style="background-color:#C8C8C8; text-align:center;">
-      <h3>Servicios</h3>
-      <img src="assets/img/columnas.png"  max-width="100%" height="100">
-      <p>
          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-          <center> <a class="btn btn-primary btn-sm">ver más...</a></center>
+                  <center> <a class="btn btn-primary btn-sm">ver más...</a></center>
 
-      </p>
+              </p>
+        </div>
+    </div>
+    <div class="col-md-4 columna">
+      <div class="col-md-12 columna" style="background-color:#C8C8C8;">
+
+              <h3 class="text-center">Servicios</h3>
+              <img class="img-responsive"  src="assets/img/columnas.png" max-width="100%" height="auto">
+              <p class="text-center">
+
+         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                  <center> <a href="servicios" class="btn btn-primary btn-sm">ver más...</a></center>
+
+              </p>
+        </div>
 
     </div>
-    <div class="col-md-4 columna" style="background-color:#C8C8C8; text-align:center;">
-      <h3>Cotice en línea</h3>
-      <img src="assets/img/columnas.png" max-width="100%" height="100">
-        <p>
-           Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
-          <center> <a class="btn btn-primary btn-sm">ver más...</a></center>
-        </p>
+    <div class="col-md-4 columna">
+      <div class="col-md-12 columna" style="background-color:#C8C8C8;">
+
+              <h3 class="text-center">Cotice en línea</h3>
+              <img class="img-responsive"  src="assets/img/columnas.png" max-width="100%" height="auto">
+              <p class="text-center">
+
+         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                  <center> <a class="btn btn-primary btn-sm">ver más...</a></center>
+
+              </p>
+        </div>
 
     </div>
 
