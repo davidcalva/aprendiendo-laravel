ab79b3b7e6c2509481c57dd80939ae487e0d3d56
gbeto <gbeto23@gmail.com>
2014-03-13 20:03:23 -0600

    contacto

--- app/views/contacto.blade.php
+++ app/views/contacto.blade.php
@@ -28,10 +28,7 @@
 			<iframe width="640" height="480" class="responsive" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/maps?ie=UTF8&amp;q=Grupo+Siel+Bombas+y+Equipos+Agroindustriales&amp;fb=1&amp;gl=mx&amp;hq=grupo+siel+cancun&amp;cid=11928730461720170746&amp;ll=21.169427,-86.83606&amp;spn=0.006295,0.006295&amp;t=m&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com.mx/maps?ie=UTF8&amp;q=Grupo+Siel+Bombas+y+Equipos+Agroindustriales&amp;fb=1&amp;gl=mx&amp;hq=grupo+siel+cancun&amp;cid=11928730461720170746&amp;ll=21.169427,-86.83606&amp;spn=0.006295,0.006295&amp;t=m&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left">Ver mapa más grande</a></small>
 
 		</div>
-		<div class="col-md-6 ">
-			<p>sielcancun@prodigy.net.mx <br>
-			sielcancun@hotmail.com</p>
-		</div>	
+			
 	</div>				
 </div>
 <div class="row fondoWhite">
