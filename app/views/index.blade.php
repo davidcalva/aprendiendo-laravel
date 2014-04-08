6d8b14da13a1d4fcab7f1d1781f392e413007cae
gbeto23 <gbeto23@gmail.com>
2014-04-07 23:59:20 -0500

    ahora ya funcionan llos indicadores del slider

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,10 +10,10 @@ http://twitter.github.com/bootstrap/javascript.html#carousel -->
 	<div class="col-md-12 " style="padding-left: 0;padding-right: 0; margin-bottom: 10px;">
 		<div id="this-carousel-id" class="carousel slide"><!-- class of slide for animation -->
 			<ol class="carousel-indicators">
-				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1" ></li>
-				<li data-target="#myCarousel" data-slide-to="2" ></li>
-				<li data-target="#myCarousel" data-slide-to="3" ></li>
+				<li data-target="#this-carousel-id" data-slide-to="0" class="active"></li>
+				<li data-target="#this-carousel-id" data-slide-to="1" ></li>
+				<li data-target="#this-carousel-id" data-slide-to="2" ></li>
+				<li data-target="#this-carousel-id" data-slide-to="3" ></li>
 			</ol>
 
 			<div class="carousel-inner">
