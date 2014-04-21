80e7f00760922b99a7cecefc6e7f95af7aa1035d
davidcalva <l.d.calva@gmail.com>
2014-04-21 17:29:04 -0500

    Se corrigio el acceso a los modulos cuando se consultaba a la bd por que en el hosting eliminaba las variables de session

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,87 +1,86 @@
 <?php  
+/**
+* 
+*/
 
 class LoginController extends BaseController {
 
-	/**
-	* Display a listing of the resource.
-	*
-	* @return Response
-	*/
-
-	public function index(){
-		if (Auth::check())
+    /**
+    * Display a listing of the resource.
+    *
+    * @return Response
+    */
+   
+    public function index(){
+        if (Auth::check())
         {
-			return Redirect::to('/panelAdmin');
-		}
-		return View::make('admin/login');
-	}
-	public function doLogin(){
-		// Guardamos en un arreglo los datos del usuario.
+            return Redirect::to('admin/panelAdmin');
+        }
+        return View::make('admin/login');
+    }
+    public function doLogin(){
+        // Guardamos en un arreglo los datos del usuario.
         $userdata = array(
             'email' => Input::get('email'),
             'password' => Input::get('password')
         );
         // Validamos los datos y además mandamos como un segundo parámetro la opción de recordar el usuario.
+
         if(Auth::attempt($userdata, Input::get('remember-me', 0)))
         {
-            // De ser datos válidos creamos arreglo de sesion con los accesos a los modulos
+             
+            
+           // De ser datos válidos creamos arreglo de sesion con los accesos a los modulos
             $usuario = Usuarios::where('email','=',$userdata['email'])->first();
-
+            
             #validamos que el usuario exista 
             if ( is_null ($usuario) )
             {
-                echo "Usted no tiene permisos: usuario no encontrado";
-                exit;
+                #echo "Usted no tiene permisos: usuario no encontrado";
+                #exit;
+                return Redirect::route('loginIndex')->with('mensaje_error', 'Usted no tiene permisos: usuario no encontrado');
+            }
+            if( is_null($usuario->perfil_id) ){
+                #echo 'No tiene perfil';
+                #exit;
+                return Redirect::route('loginIndex')->with('mensaje_error', 'Usted no tiene permisos: Sin perfil.');
             }
             $nombreUsuario = $usuario->nombres;
             $idUsuario     = $usuario->id;
-            #valdiamos que tenga perfil
-            if( is_null($usuario->perfil_id) ){
-                echo 'No tiene perfil';
-                exit;
-            }
-            #obtenemos el perfil
-            $perfil = Perfiles::find($usuario->perfil_id);
-
-            #comprabamos que se encontro el perfil
-            if(is_null($perfil)){
-                echo 'Sin permisos';
-                exit;
-            }
-            $usurioPerfil = strtolower($perfil->perfil);
-            #modulos del perfil
-            $modulos = $perfil->perfilAccesos;#->toArray();
-            #verificamos que el perfil tenga algun modulo
-            if(is_null($modulos)){
-                echo "No tiene permisos a ningun modulo";
-                exit;
+            $idPerfilUser  = $usuario->perfil_id;
+             #valdiamos que tenga perfil
+            $modelPerfilModulos = new PerfilModulosPDO;
+            $modulos = $modelPerfilModulos->getModulos($idPerfilUser);
+            if( ! $modulos ){
+                return Redirect::route('loginIndex')->with('mensaje_error', 'Usted no tiene permisos: Sin modulos.');
             }
             
             
-
             foreach ($modulos as $modulo) {
-                $arrModulos[$modulo->mAlias] = array('lectura'   => $modulo->pivot->lectura,
-                                                      'escritura' => $modulo->pivot->escritura
+                $arrModulos[$modulo['mAlias']] = array('lectura'   => $modulo['lectura'],
+                                                      'escritura' => $modulo['escritura']
                                                       ); 
             } 
-            Session::flush();
             Session::put('modulosAcceso', $arrModulos);
             Session::put('tiempoInicio', time());
-            Session::put('datosUsuario',array('idUsuario' => $idUsuario,'nombre' => $nombreUsuario, 'perfil' => $usurioPerfil));
+            Session::put('datosUsuario',array('idUsuario' => $idUsuario,'nombre' => $nombreUsuario, 'perfil' => strtolower($modulos[0]['perfil']) ) );
+         
+            
             return Redirect::route('panelAdmin');
-           
         }
         else{
-            /*return Redirect::to('login')
-                    ->with('mensaje_error', 'Tus datos son incorrectos')
-                    ->withInput();*/
-
-            return Redirect::route('loginIndex')->with('mensaje_error', 'Tus datos son incorrectos');
+            #return Redirect::to('login')
+            #        ->with('mensaje_error', 'Tus datos son incorrectos')
+            #        ->withInput();
+            #echo "no login";
+            #exit;
+            return Redirect::route('loginIndex')->with('mensaje_error', 'Datos  incorrectos');
             #echo "datos incorrectos";
         }
+
         // En caso de que la autenticación haya fallado manda un mensaje al formulario de login y también regresamos los valores enviados con withInput().
        
-	}
+    }
 
     public function doLogout(){
         Session::flush();
