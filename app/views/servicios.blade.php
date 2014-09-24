2234378caced9e15bfc2b1b4d827b76a880203a7
gbeto23 <gbeto23@gmail.com>
2014-09-24 00:43:12 -0500

    cambios en el diseño

--- app/views/servicios.blade.php
+++ app/views/servicios.blade.php
@@ -6,25 +6,28 @@ Servicios
 @stop
 @section ('content')
 <div class="row fondoWhite">
-	<div class="col-md-12">
-		
+		<div class="col-md-12">
+			<div class="col-md-7">
 
-		<h2>Nuestros servicios<h2>
-		<h3>“Mantenimiento y ejecución de proyectos”</h3>
-		 <img  class="img-responsive"src="assets/img/servicios.gif"  alt="Servicios" max-width="100%" height="242" />
-		 <p class="text-left">
-		 	Desde la mera instalación nos comprometemos a que el funcionamiento de todos los equipos que ofrecemos sea eficaz. 
-		 	Tambien ofrecemos mantenimiento correctivo y preventivo, y asea a corto, mediano o largo plazo.
-		 	Contamos con:
-			<ul class="" style="font-size:18px; color:#787878;">
-					<li>Bombeo</li>
-					<li>Ventilación</li>
-					<li>Instalación Eléctrica</li>
-					<li>Fabricación de Ductería</li>
-					<li>Paileria y Soldadura</li>
-		 	</ul>	
-		 </p>
-	
+			<h2>Nuestros servicios<h2>
+			
+			 <p class="text-left">
+			 	Desde la mera instalación nos comprometemos a que el funcionamiento de todos los equipos que ofrecemos sea eficaz. 
+			 	Tambien ofrecemos mantenimiento correctivo y preventivo, y asea a corto, mediano o largo plazo.
+			 	Contamos con:
+				<ul class="" style="font-size:18px; color:#787878;">
+						<li>Bombeo</li>
+						<li>Ventilación</li>
+						<li>Instalación Eléctrica</li>
+						<li>Fabricación de Ductería</li>
+						<li>Paileria y Soldadura</li>
+			 	</ul>	
+			 </p>
+		</div>
+		<div class="col-md-5">
+						 <img  class="img-responsive"src="assets/img/servicios.png"  alt="Servicios" max-width="100%" height="242" />
+
+		</div>
 	</div>
 </div>
 @stop
