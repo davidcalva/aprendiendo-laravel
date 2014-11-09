918ccdc8bdc6d44109ec99fbe364537cb9ccb310
gbeto23 <gbeto23@gmail.com>
2014-11-09 15:02:45 -0600

    cambios

--- app/views/admin/layoutAdmin.blade.php
+++ app/views/admin/layoutAdmin.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
+		<link rel="icon" type="image/png" href="assets/img/favicon.ico" />
     	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	{{-- Bootstrap --}}
